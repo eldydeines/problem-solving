1930590e52cac799cd0280bd40e43f1e4f07b644
Eldy Deines <eldy.deines@outlook.com>
2021-10-29 12:56:03 -0600

    added jest module and scripted construct-note

--- frequency-multiple/construct-note.js
+++ frequency-multiple/construct-note.js
@@ -1,3 +1,31 @@
-// add whatever parameters you deem necessary
-function constructNote() {
+// Write a function called constructNote, which accepts two strings, a message and some letters. 
+// The function should return true if the message can be built with the letters that you are given; 
+// otherwise, it should return false.
+// Assume that there are only lowercase letters and no space or special characters in both the message and the letters.
+// Complexity: O(M + N) - If M is the length of message and N is the length of letters
+
+function frequencyCounter(str) {
+
+    let frequencies = new Map();
+
+    for (let char of str) {
+        let charCount = frequencies.get(char) || 0;
+        frequencies.set(char, charCount + 1);
+    }
+    return frequencies;
+}
+
+function constructNote(message, strInventory) {
+
+    if (message.length > strInventory.length) return false;
+
+    let messageMap = frequencyCounter(message);
+    let inventoryMap = frequencyCounter(strInventory);
+
+    for (let key of messageMap.keys()) {
+        if (messageMap.get(key) !== inventoryMap.get(key)) {
+            return false;
+        }
+    }
+    return true;
 }
